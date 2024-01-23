c4adf9990657b28b013e7c3341adfdb884e32139
cgarcia <cgarcia@promax.es>
2024-01-23 08:52:38 +0100

    feat[config]: add enviroment config

--- db.js
+++ db.js
@@ -1,11 +1,13 @@
-import {createConnection} from 'mysql2/promise';
+import { createConnection } from 'mysql2/promise';
+
+import { DB_HOST, DB_DATABASE, DB_PASSWORD, DB_PORT, DB_USER } from './utils/config';
 
 const connection = await createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    port: 3306,
-    database: 'moviesdb'
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  port: DB_PORT,
+  database: DB_DATABASE,
 });
 
 export default connection;

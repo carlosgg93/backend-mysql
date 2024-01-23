2d339c205602fff22449fa93596b0e2dff2e939a
cgarcia <cgarcia@promax.es>
2024-01-23 07:24:59 +0100

    fix[userModel]: correct import behaviour

--- models/userModel.js
+++ models/userModel.js
@@ -1,50 +1,42 @@
-import connection from "../db.js";
+import connection from '../db.js';
 
-export class UserModel {
+export default class UserModel {
   static async getAllUsers() {
-    const [rows] = await connection.query(
-      'SELECT * FROM users'
-    );
+    const [rows] = await connection.query('SELECT * FROM users');
     return rows;
   }
 
   static async getUserById(id) {
-    const [rows] = await connection.query(
-      'SELECT * FROM users WHERE id = ?',
-      [id]
-    );
+    const [rows] = await connection.query('SELECT * FROM users WHERE id = ?', [id]);
     return rows[0];
   }
 
   static async getUserByUserName(username) {
-    const [rows] = await connection.query(
-      'SELECT * FROM users WHERE username = ?',
-      [username]
-    );
+    const [rows] = await connection.query('SELECT * FROM users WHERE username = ?', [username]);
     return rows[0];
   }
 
   static async deleteUserById(id) {
-    const [rows] = await connection.query(
-      'DELETE FROM users WHERE id = ?',
-      [id]
-    );
+    const [rows] = await connection.query('DELETE FROM users WHERE id = ?', [id]);
     return rows;
   }
 
   static async addUser(user) {
-    const [rows] = await connection.query(
-      'INSERT INTO users (username, email, password) VALUES (?, ?, ?)',
-      [user.username, user.email, user.password]
-    );
+    const [rows] = await connection.query('INSERT INTO users (username, email, password) VALUES (?, ?, ?)', [
+      user.username,
+      user.email,
+      user.password,
+    ]);
     return rows;
   }
 
   static async updateUserById(id, user) {
-    const [rows] = await connection.execute(
-      'UPDATE users SET username = ?, email = ?, password = ? WHERE id = ?',
-      [user.username, user.email, user.password, id]
-    );
+    const [rows] = await connection.execute('UPDATE users SET username = ?, email = ?, password = ? WHERE id = ?', [
+      user.username,
+      user.email,
+      user.password,
+      id,
+    ]);
     return rows;
   }
 }
